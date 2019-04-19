e8d1ca83c883c92e3b9e89abd4bc6a46403d0d90
MRoO1 <rah-mp96@outlook.fr>
2019-04-19 03:22:22 +0100

    student app

---
+++ firstApp/resources/views/show_students.blade.php
@@ -0,0 +1,10 @@
+@extends('studentApp.layout')
+
+
+@section('content')
+
+
+<!-- from here to layout -->
+<h1>content </h1>
+
+@endsection
---
+++ firstApp/resources/views/studentApp/layout.blade.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <title>Student App</title>
+
+    <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+    <!--
+    	by using balde templating engine we can inject a php  vriable ,
+    	functions and a lot  morething  in html view files 
+
+    <?php //include()
+    ?> -->
+  </head>
+  <body class="d-flex flex-column h-100">
+    <header>
+
+
+
+
+
+
+
+
+
+  <!-- Fixed navbar -->
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <a class="navbar-brand" href="#">{{config('app.name')}}</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Students</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Courses</a>
+        </li>
+      </ul>
+      <form class="form-inline mt-2 mt-md-0">
+        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+</header>
+
+
+
+
+
+
+
+<main>
+<br><br><br>
+  <div></div>@yield('content')
+
+</main>
+
+
+
+
+
+
+<footer class="footer mt-auto py-3">
+  <div class="container">
+    <span >Place sticky footer content here.</span>
+  </div>
+</footer>
+</body>
+</html>
